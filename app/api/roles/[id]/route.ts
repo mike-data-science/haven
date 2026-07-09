7300ed16e3c976428fcb59e6524900bb285de284
a@a.a <gamedevelopermike@gmail.com>
2026-07-09 12:06:12 +0300

    fix_roles

--- app/api/roles/[id]/route.ts
+++
@@ -1,5 +0,0 @@
-import { roleHandlers } from "@/lib/apiEntities";
-
-export const GET = roleHandlers.GET_BY_ID;
-export const PUT = roleHandlers.PUT;
-export const DELETE = roleHandlers.DELETE;
--- app/api/roles/route.ts
+++
@@ -1,4 +0,0 @@
-import { roleHandlers } from "@/lib/apiEntities";
-
-export const GET = roleHandlers.GET;
-export const POST = roleHandlers.POST;
